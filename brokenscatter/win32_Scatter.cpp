45ba0fd239d49974d23b280c5ad16d5b140b2307
Oskar Mendel <brokenprogrammer@gmail.com>
2019-02-13 12:51:44 +0100

    Initial win32 window.

---
+++ brokenscatter/win32_Scatter.cpp
@@ -0,0 +1,87 @@
+#include <Windows.h>
+
+LRESULT CALLBACK
+Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+	LRESULT Result = 0;
+
+	switch (Message)
+	{
+		case WM_SIZE:
+		{
+		} break;
+
+		case WM_CLOSE:
+		{
+			PostQuitMessage(0);
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+		} break;
+
+		case WM_SYSKEYDOWN:
+		case WM_SYSKEYUP:
+		case WM_KEYDOWN:
+		case WM_KEYUP:
+		{
+		} break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+
+			//TODO: This is temp
+			FillRect(DeviceContext, &Paint.rcPaint, (HBRUSH)(COLOR_WINDOW + 25));
+
+			EndPaint(Window, &Paint);
+		} break;
+
+		default:
+		{
+			Result = DefWindowProcA(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return (Result);
+}
+
+int CALLBACK
+WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmpLine, int nCmdShow)
+{
+	WNDCLASSA WindowClass = {};
+
+	WindowClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC; //TODO: Might need to change.
+	WindowClass.lpfnWndProc = Win32MainWindowCallback;
+	WindowClass.hInstance = hInstance;
+	WindowClass.lpszClassName = "BrokenScatter";
+
+	if (RegisterClassA(&WindowClass))
+	{
+		HWND Window = CreateWindowExA(0, WindowClass.lpszClassName, "BrokenScatter", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL,
+									  NULL, // TODO: MENU
+									  hInstance, NULL);
+
+		if (Window)
+		{
+			//TODO: Code in this scope is temporary.
+			ShowWindow(Window, nCmdShow);
+
+			MSG msg = {};
+			while (GetMessage(&msg, NULL, 0, 0))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+	}
+
+	return (0);
+}
